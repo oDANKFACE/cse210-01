16f8fee4d77d8d11b39a4af66e1c1a360db8cd60
oDANKFACE <darien.r.weller@gmail.com>
2022-09-26 22:29:04 -0600

    Completed the check_victory and check_tie functions. Sharpened up some output.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -7,19 +7,20 @@ Project: tic_tac_toe
 
 
 def main():
+    print("Welcome to Tic-Tac-Toe!")
     game_board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     turn = ""
     victory = False
     tie_game = False
     while not (victory or tie_game):
         draw_board(game_board)
-        # check_victory()
-        # if victory:
-        #     print(f"Great job {turn}'s, you win!")
-        #     break
-        # check_tie()
-        # if tie_game:
-        #     print("Tie Game. You're both losers.")
+        victory = check_victory(game_board)
+        if victory:
+            print(f"The {turn}'s win!")
+            break
+        tie_game = check_tie(game_board)
+        if tie_game:
+            print("Tie Game.")
         turn = switch_player(turn)
         choice = int(input(f"{turn}'s turn to choose a square (1-9): "))
         update_board(game_board, choice, turn)
@@ -47,12 +48,25 @@ def switch_player(player):
         return "x"
 
 
-def check_victory():
-    pass
+def check_victory(board):
+    if ((board[8] == board[4] == board[0]) or
+            (board[6] == board[4] == board[2]) or
+            (board[0] == board[1] == board[2]) or
+            (board[3] == board[4] == board[5]) or
+            (board[6] == board[7] == board[8]) or
+            (board[0] == board[3] == board[6]) or
+            (board[1] == board[4] == board[7]) or
+            (board[2] == board[5] == board[8])):
+        return True
+    else:
+        return False
 
 
-def check_tie():
-    pass
+def check_tie(board):
+    for num in board:
+        if num != "x" and num != "o":
+            return False
+    return True
 
 
 if __name__ == "__main__":
